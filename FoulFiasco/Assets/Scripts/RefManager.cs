047912608e7d8ce4916065780c270f51daf73891
Alkan Cakir <acakir07@student.rocvantwente.nl>
2024-05-13 16:41:49 +0200

    Feat: Ref follow
    ref springt nu niet maar maakt dingen kapot

--- FoulFiasco/Assets/Scripts/RefManager.cs
+++ FoulFiasco/Assets/Scripts/RefManager.cs
@@ -1,12 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RefManager : MonoBehaviour
 {
+    [SerializeField] GameObject explosion;
+    GameObject explosionGameObject;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Wall" || collision.tag == "Ball")
         {
-
+            transform.position = new Vector3(-8, transform.position.y, transform.position.z);
+            explosionGameObject = Instantiate(explosion);
+            explosionGameObject.transform.position = collision.transform.position;
+            Destroy(collision.gameObject);
+            StartCoroutine(DestoryParticle());
         }
+    }
+
+    IEnumerator DestoryParticle()
+    {
+        yield return new WaitForSeconds(0.6f);
+        Destroy(explosionGameObject);
     }
 }
