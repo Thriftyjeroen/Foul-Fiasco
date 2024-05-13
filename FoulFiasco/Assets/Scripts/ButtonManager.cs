2b38d7fa9ad81649e048f8a03387c158f81dda38
Daniël Kosters <0356076@student.rocvantwente.nl>
2024-05-13 16:09:41 +0200

    misc: bescrijvende commends
    ik heb de beschrijvende commends in de scripts die ik heb geschreven toegevoegd waar vergeten en vertaald naar engels.
    70 regels code vervangen voor een inputfield

--- FoulFiasco/Assets/Scripts/ButtonManager.cs
+++ FoulFiasco/Assets/Scripts/ButtonManager.cs
@@ -5,23 +5,23 @@ public class ButtonManager : MonoBehaviour
 {
     public void Start()
     {
-        if (!(PlayerPrefs.HasKey("Coins"))) PlayerPrefs.SetInt("Coins", 3);
+        if (!(PlayerPrefs.HasKey("Coins"))) PlayerPrefs.SetInt("Coins", 3);//if there is no playerPref with the key Coins it wil be crated and given 3 coins
     }
-    public Scene enumType;//hier word de scene bepaald op basis van de scene die geselecteerd word in de inspector van de gameObject waar dit script aan verbonden is
+    public Scene enumType;//the scene wil be selected by the user/developer in the inspector and saved in this variable
     public void ChangeScene()
     {
-        if (!((int)enumType == 3 && !(PlayerPrefs.GetInt("Coins") > 0)))//zolang de PlayerPrefs (wat
+        if (!((int)enumType == 3 && !(PlayerPrefs.GetInt("Coins") > 0)))//simply said, if the interger of the selected enum is not 3 and the coins is not higher than 0 the sene will not be changed
         {
-            SceneManager.LoadScene((int)enumType);//de loadscene van scenemanager laat de aangegeven scene met als input een string of een interger, en sinds er een enum gebruiken gebruiken we de enum die verbonden is met de variabele enumType en daar de waarde van.
+            SceneManager.LoadScene((int)enumType);//the scene will be loaded based of the integer of the enum that is chosen in the inspector
         }
     }
-    public void LeaveGame()//deze method zorgt dat de aplicatie af gesloten kan worden op basis van een knop (wat deze method laat runnen)
+    public void LeaveGame()//this method will quit the application
     {
         Application.Quit();
     }
-    public enum Scene//dit is een enumerator, hier kan je een aantal enums(soort variabele) maken met een waarde als integer. die werkt als een array of list, maar het verschil is dat je die ook kan veranderen op de manier die is uitgequote. ook is het zo dat als je een variabele maakt zoals op regel 7 met de naam van de enum en vervolgens het script verbind met een gameObject dat je dan tussen de enums kan kiezen via de inspector
-    {//als je een nieuwe enum maakt zorg dan dat je een comma plaats tussen de enum met de laatste enum geen comma erachter
-        MainMenu/* = (getal naar keuze)*/,
+    public enum Scene//in the enumarator u can make a group of values that has a name (keyword) for every keyword u make it will get automaticly a value that works like the index of a list of array. bud the diverence is that u can change the value that is shown by the first keyword where it is quoted out. u can also select the keyword and value with the code on line 11 where in the inspector only the keywords will be shown
+    {//if u make a enum u have to write a , at the end exept the last one
+        MainMenu/* = (number of choise)*/,
         CoinMaster,
         Credits,
         Game,
--- FoulFiasco/Assets/Scripts/RekenenScript.cs
+++ FoulFiasco/Assets/Scripts/RekenenScript.cs
@@ -1,41 +1,38 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RekenenScript : MonoBehaviour
 {
+    //in this field will be a bunch of variables decleared that will be used in this scirpt.
     [SerializeField] TMP_Text text;
-    [SerializeField] TMP_Text answerText;
+    [SerializeField] TMP_InputField inputField;
     List<string> sum = new List<string>();
     bool operatorFound = false;
     string sSum = "";
     bool correctValue;
     string answer = "";
-    List<string> input = new List<string>();
-    string userAnswer = "";
-    // Start is called before the first frame update
-    void Start()
+    void Start()//when the sene start with this script this code will run
     {
-        SumGenerator();
-        text.text = sSum;
-        CalculationOrder();
-        float temperarly = float.Parse(sum[0]);
-        print(temperarly);
-        answer = Mathf.Round(temperarly).ToString();
-        print(answer);
+        SumGenerator();//here will be a sum generated
+        text.text = sSum;//the text of the sum will be displayed on the text from TextMeshPro(TMP)
+        CalculationOrder();//the sum needs to be calculated bevore the user can give input.
+        float temperarly = float.Parse(sum[0]);//i knew that some anser could be a decimal. so i parsed it to a temperarly variable to use the answer more than once 
+        print(temperarly);//to make it possible for the developer to check the value (just in case something go's wrong) i made the temperarly value print in the console
+        answer = Mathf.Round(temperarly).ToString();//the round funktion/method only accept float as a arguments, so i had to parse the answer of the sum to a float. and the answer is a string so i used a ToString method for that.(yes i could use the float or int as a answer, bud sinds the user can use every butten as a answer i think it is smarter to use a string to compare the values later
+        print(answer);//for testing purposes it is way faster to get the answer already printed in the console insted of calculating it first
     }
     void SumGenerator()
     {
-        for (int times = UnityEngine.Random.Range(2, 8); times > 0; times--)
+        for (int times = UnityEngine.Random.Range(2, 5); times > 0; times--)//the sum will have a random length, from 2 values to 4 values
         {
-            sum.Add(UnityEngine.Random.Range(1, 101).ToString());
-            print(sum.Last());
-            switch (UnityEngine.Random.Range(1, 5))
+            sum.Add(UnityEngine.Random.Range(1, 101).ToString());//the sum will get a random number as a string
+            print(sum.Last());//for testing purpose the generated number will be printed to the console
+            switch (UnityEngine.Random.Range(1, 5))//based on the random number a opperator will be chosen and added to the sum
             {
                 case 1:
                     sum.Add("*");
@@ -54,8 +51,8 @@ public class RekenenScript : MonoBehaviour
                     break;
             }
         }
-        sum.RemoveAt(sum.Count() - 1);
-        foreach (string sumValue in sum)
+        sum.RemoveAt(sum.Count() - 1);//if the sum is generated there will be a opperator at the end witch would break the application, so i remove it from the sum
+        foreach (string sumValue in sum)//i wand the sum beïng shown in the aplication when it is generated, so i loop trough the sum and add it to the sSum with a space between them to make a variable that cani printed in the TMP text
         {
             sSum += " " + sumValue;
         }
@@ -63,34 +60,28 @@ public class RekenenScript : MonoBehaviour
     void CalculationOrder()
     {
         int lokation;
-        do//de do-while loop runt minimaal 1 keen en zolang de contie in de statement van while staat word de code in de loop weer geloopt
+        do//the do-while loop runs at least once and as long the contion in the while statement is true, the code in the loop will run again
         {
-            operatorFound = false;// er kan meerdere malen door een lijst heen geloopt worden. voor elke keer moet deze variabele wel weer op false gezet worden
-            lokation = 0;// de lokatie word  elke keer weer op 0 gezet aangezien je anders door gaat tellen terwijl er weer vanaf het begin weer word geloopt, maar om de lokatie van de operator te achterhalen moet de waarde van deze lokatie het zelfde zijn als hoeveelste waarde de foreach loop is, anders is de lokatie dus buiten de lengte van de lijst wat dus niet kan.
-            foreach (string value in sum)//er word door de lijst met waardes heen geloopt
+            operatorFound = false;//it is possible that there is more than one opperator in the operator, so if this loop tries to search for another operator it needs this bool on flase because it is not found jet
+            lokation = 0;// the location is reset to 0 every time, otherwise you will continue counting while start searching again from the beginning which means that the next lokation that is found will be outside of the indext of the sumlist, wich is not possible. and the variable will let the script later know where the operator is
+            foreach (string value in sum)//there will be looped trough the sum list
             {
-                switch (value)//hier word gekeken of de waarde een operator is
+                switch (value)//here will be checked if the value is a opperator 
                 {
                     case "*":
                     case "/":
                     case "%":
-                        operatorFound = true;//als er een operator gevonden is die gezocht word word de operatorFound op true gezet
+                        operatorFound = true;//if the opperator is found the variable will be set true to let the script later know to run or don't run certain code
                         break;
                     default:
                         break;
                 }
-                if (!(operatorFound))//zo lang er geen operator is gevonden word de lokatie met 1 verhoogd
-                {
-                    lokation++;//je zou zeggen als de lokatie met 1 groter word totdat de operator word gevonden dat je er dan 1 naast zit, maar het lokaliseren van een waarde in een list begint op 1, waardoor je dan gelijk de waarde hebt die vertelt welke operator het  is
-                }
+                if (!(operatorFound)) lokation++;//if the operator is not found the value of the lokation will be added with 1
             }
-            Thread.Sleep(100);
-            if (operatorFound)//als er een operator is gevonden word deze code gerunt
-            {
-                Calculate(lokation);//hier word de Calculate method opgeroepen en word de lokatie van de operator mee gegeven
-            }
-        } while (sum.Contains("*") || sum.Contains("/") || sum.Contains("%"));
-        do// dit is exact de zelfde code als in de loop hierboven, maar dan met een andere method die word opgeroepen en worden er andere operators gezogt, ja ik kon dat allemaal in 1 keer doen, maar er bestaat iets als de reken volg orde, dan doe je eerst de berekeningen van *,/,% en dan de berekeningen van + en -. Dus door die arpart te doen achter elkaar houd de aplicatie zich aan de reken volgorde
+            Thread.Sleep(100);//i beleve the aplication was running to fast so with the tread.sleep it will slow it down with 0.1 secconds so the next code will be runned
+            if (operatorFound) Calculate(lokation);//if there is a operatorFound it will be calculated in the calculate method with the lokation as a argument
+        } while (sum.Contains("*") || sum.Contains("/") || sum.Contains("%"));//if there is 1 of there opperators stil in the sum list it will loop again
+        do//this is exactly the same as the code above, bud than for the + and - opperators, and yeah i could do it bouth at once, bud i wand to use the calculation order witch means /,%,* first than +,-
         {
             operatorFound = false;
             lokation = 0;
@@ -117,7 +108,7 @@ public class RekenenScript : MonoBehaviour
             }
         } while (sum.Contains("+") || sum.Contains("-"));
     }
-    void CalculateCountingOperators(int pLokation)// je ziet een p staan voor lokation, en dat hout in dat de variabele een parameter van lokation is, dus die ontvangt de waarde van lokation. verder is deze method exact het zelfde als de Calculate method, maar dan voor + en -
+    void CalculateCountingOperators(int pLokation)//this method is exactly the same as the calculate method bud then for + and -
     {
         correctValue = true;
         if (sum[pLokation] == "+")
@@ -138,78 +129,35 @@ public class RekenenScript : MonoBehaviour
             sum.RemoveAt(pLokation);
         }
     }
-    void Calculate(int pLokation)// in deze method worden de *,/,% berekent met de meegegeven waarde
+    void Calculate(int pLokation)// in deze method worden de *,/,% berekent met de meegegeven waarde//in this method the part of the sum with *,/,% will be calculated based on the lokation that is given 
     {
-        correctValue = true;//de correctValue word op true gezet aangezien de waarde nu (als het goed is nu klopt voor de berekening)
-        if (sum[pLokation] == "*")//hier word gekeken of de waarde * (vermedigvuldig teken) is en worden de 2 aan grenzende waardes berekent. hieronde de uitleg in detail met | alsgrens voor elk stukje van de code
-        {// we willen niet dat elke    |de waarde voor de * om gezet naar een   |de waarde na de * die ook word omgezet naar| natuurlijk is de type 
-         // keer deze code word gerunt |data type die met comma getallen om     |een type die comma getallen kan verwerken..|Double niet de zelfde als 
-         // met de zelfde waardes,     |kan gaan en die word vermedigvuldigd    |                                           |een string zoals die in de 
-         // dus die worden vervangen   |met...                                  |                                           |list, daarom word na de 
-         // met het andwoord van deze  |                                        |                                           |berkening het andwoord 
-         // berekening                 |                                        |                                           |omgezet naar een string
+        correctValue = true;//the correctValue is set to true since the value (if correct) is now correct for the calculation
+        if (sum[pLokation] == "*")//Here it is checked whether the value is * (multiply sign) and the 2 adjacent values ​​are calculated. below the explanation in detail with | as a boundary for each piece of code
+        {//We don't want this code to  |the value for the * is converted to a |the value after the *that is also converted |of course the Double type is not
+         //be run with the same values |data type that can handle decimal     |to a type that can handle comma numbers..   |the same as a string like the 
+         //​​every time, so they are     |numbers and is multiplied by...       |                                            |one in the list, so after the 
+         //replaced with the answer to |                                      |                                            |calculation the answer is 
+         //this calculation            |                                      |                                            |converted to a string
             sum[pLokation] = (Convert.ToDouble(sum[pLokation - 1]) * Convert.ToDouble(sum[pLokation + 1])).ToString();
         }
-        else if (sum[pLokation] == "%")//wat hierboven gebeurd word ook hier gedaan, maar inplaats van vermedigvuldigen is het modulo
+        else if (sum[pLokation] == "%") sum[pLokation] = (Convert.ToDouble(sum[pLokation - 1]) % Convert.ToDouble(sum[pLokation + 1])).ToString();//What happened above is also done here, but instead of multiplying it is modulo, maar inplaats van vermedigvuldigen is het modulo
+        else if (sum[pLokation] == "/") sum[pLokation] = (Convert.ToDouble(sum[pLokation - 1]) / Convert.ToDouble(sum[pLokation + 1])).ToString();//The same is done here too, but then the values ​​are devided with each other
+        else correctValue = false;//there is also a chance that things can go wrong, then the correctValue or false is set
+        if (correctValue)//if the value is correct, the other values ​​are also removed since they have already been calculated. In terms of code it can look strange, I will explain it to you there
         {
-            sum[pLokation] = (Convert.ToDouble(sum[pLokation - 1]) % Convert.ToDouble(sum[pLokation + 1])).ToString();
-        }
-        else if (sum[pLokation] == "/")// ook hier word het zelfde gedaan, maar dan worden de waardes met elkaar gedeelt
-        {
-            sum[pLokation] = (Convert.ToDouble(sum[pLokation - 1]) / Convert.ToDouble(sum[pLokation + 1])).ToString();
-        }
-        else//er is ook een kans dat het mis kan gaan, dan word de gebruiker erover geinvormeer
-        {
-            correctValue = false;// hier word de variabele correcteValue op false gezet aangezien er inderdaat niet de juiste waarde is gevonden op de een of andere manier
-        }
-        if (correctValue)// als de waarde correct is worden ook de andere waardes weg gehaald aangezien die al berekent zijn. kwa code kan het gek uit zien, ik leg het je daar dan uit
-        {
-            sum.RemoveAt(pLokation - 1);//eerst word de waarde voor het antwoord verwijderd,
-            sum.RemoveAt(pLokation);//daarna de waarde op de lokatie van het antwoord, maar dat klinkt gek, maar het klopt wel. aangezien de waarde voor het antwoord is verwijderd worden alle waardes met 1 naar voren geschoven. dus het antwoord komt op de oude lokatie van de waarde voor het antwoord, en de waarde na het antwoord komt op de oude lokatie van het antwoord en door de lokatie van het oude antwoord te verwijderen verwijderen we de waarde achter het antwoord.
+            sum.RemoveAt(pLokation - 1);//first the value for the answer is removed
+            sum.RemoveAt(pLokation);//then the value at the location of the answer, but that sounds crazy, but it is correct. Since the value for the answer has been removed, all values ​​are shifted forward by 1. so the answer goes to the old location of the value before the answer, and the value after the answer goes to the old location of the answer and by removing the location of the old answer we remove the value after the answer.
         }
     }
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0)) ChangeList("0");
-        else if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) ChangeList("1");
-        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) ChangeList("2");
-        else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)) ChangeList("3");
-        else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4)) ChangeList("4");
-        else if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5)) ChangeList("5");
-        else if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6)) ChangeList("6");
-        else if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7)) ChangeList("7");
-        else if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)) ChangeList("8");
-        else if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9)) ChangeList("9");
-        else if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus)) ChangeList("-");
-        else if (Input.GetKeyDown(KeyCode.Backspace)) ChangeList("");
-        else if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.KeypadEnter)||Input.GetKey(KeyCode.Return))//if the enter button is pressed on the keypad or the big enter button on your keyboard (this is origanaly called the return button so that is why i use the return keycode to make it possible that the other enter butten can be used)
         {
-            PrintInput();
-            if (userAnswer == answer)
+            if (inputField.text == answer)//if the answer is the same as the value of the inputfield will the Value of the Coins playerpref will be 1 more and the mainmenu scene will be loaded
             {
                 PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 1);
                 SceneManager.LoadScene(0);
             }
         }
-    }
-    void ChangeList(string value)
-    {
-        if (value == "") input.RemoveAt(input.Count() - 1);
-        else input.Add(value);
-        PrintInput();
-    }
-    void PrintInput()
-    {
-        userAnswer = "";
-        if (input != null)
-        {
-            foreach (string number in input)
-            {
-                userAnswer += number;
-            }
-        }
-        else userAnswer = "";
-        answerText.text = userAnswer;
     }
 }
