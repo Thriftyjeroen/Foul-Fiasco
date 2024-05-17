b78ee439a66e636fb6c42616f6b805a35c3c18cf
Daniël Kosters <0356076@student.rocvantwente.nl>
2024-05-17 14:38:16 +0200

    feat: intro
    er is een korte intro scene waar een oude countdown te zien is. deze moet nog na de countdown op niet actief gezet worden

--- FoulFiasco/Assets/Scripts/ButtonManager.cs
+++ FoulFiasco/Assets/Scripts/ButtonManager.cs
@@ -11,27 +11,21 @@ public class ButtonManager : MonoBehaviour
         if (!(PlayerPrefs.HasKey("Coins"))) PlayerPrefs.SetInt("Coins", 3);
     }
     public Scene enumType;//hier word de scene bepaald op basis van de scene die geselecteerd word in de inspector van de gameObject waar dit script aan verbonden is
-    public void ChangeScene()
+    public void ChangeScene(string scene)
     {
-        if (!((int)enumType == 3 && !(PlayerPrefs.GetInt("Coins") > 0)))//zolang de PlayerPrefs (wat
+        if (!(scene == "Game" && !(PlayerPrefs.GetInt("Coins") > 0)))//zolang de PlayerPrefs (wat
         {
-            SceneManager.LoadScene((int)enumType);//de loadscene van scenemanager laat de aangegeven scene met als input een string of een interger, en sinds er een enum gebruiken gebruiken we de enum die verbonden is met de variabele enumType en daar de waarde van.
+            playerInfo.score = 0;
+            SceneManager.LoadScene(scene);//de loadscene van scenemanager laat de aangegeven scene met als input een string of een interger, en sinds er een enum gebruiken gebruiken we de enum die verbonden is met de variabele enumType en daar de waarde van.
         }
     }
     public void LeaveGame()//deze method zorgt dat de aplicatie af gesloten kan worden op basis van een knop (wat deze method laat runnen)
     {
         Application.Quit();
     }
-
-    public void ChooseScene(string pSceneName)
-    {
-        playerInfo.score = 0;
-        SceneManager.LoadScene(pSceneName);
-    }
-
-    public enum Scene//dit is een enumerator, hier kan je een aantal enums(soort variabele) maken met een waarde als integer. die werkt als een array of list, maar het verschil is dat je die ook kan veranderen op de manier die is uitgequote. ook is het zo dat als je een variabele maakt zoals op regel 7 met de naam van de enum en vervolgens het script verbind met een gameObject dat je dan tussen de enums kan kiezen via de inspector
+    /*public enum Scene//dit is een enumerator, hier kan je een aantal enums(soort variabele) maken met een waarde als integer. die werkt als een array of list, maar het verschil is dat je die ook kan veranderen op de manier die is uitgequote. ook is het zo dat als je een variabele maakt zoals op regel 7 met de naam van de enum en vervolgens het script verbind met een gameObject dat je dan tussen de enums kan kiezen via de inspector
     {//als je een nieuwe enum maakt zorg dan dat je een comma plaats tussen de enum met de laatste enum geen comma erachter
-        MainMenu/* = (getal naar keuze)*/,
+        MainMenu/* = (getal naar keuze),
         Game,
         Tutorial,
         CoinMaster,
@@ -39,5 +33,5 @@ public class ButtonManager : MonoBehaviour
         CutScene,
         Settings,
         Credits
-    }
+    }*/
 }
