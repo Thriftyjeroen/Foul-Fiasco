889decea0a681e5c67b4c3d4ea1f22cb80ebe804
THERMELINK <thermelink01@student.rocvantwente.nl>
2024-05-22 09:51:20 +0200

    CHORE: code gecommend
    commented code

--- FoulFiasco/Assets/Scripts/PlayerInfo.cs
+++ FoulFiasco/Assets/Scripts/PlayerInfo.cs
@@ -12,13 +12,16 @@ public class PlayerInfo : MonoBehaviour
 
     private void Start()
     {
+        //object does not get destroyed when a new scene is loaded
         DontDestroyOnLoad(gameObject);
     }
 
     private void Update()
     {
+        //if there is a game object with the "score" tag 
         if (GameObject.FindGameObjectWithTag("Score") != null)
         {
+            //scoreText gets assigned to the game object with the "score" tag, at the TMP_text component
             scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<TMP_Text>();
         }
     }
@@ -26,17 +29,24 @@ public class PlayerInfo : MonoBehaviour
     private void FixedUpdate()
     {
         time += Time.deltaTime;
+
+        //if the time is higher than 0,1
         if (time > 0.1f)
         {
+            //1 gets added to score
             score++;
+            //time reset to 0
             time = 0;
         }
 
+        //if there is a gameobject with the tag "score"
         if (GameObject.FindGameObjectWithTag("Score")  != null)
         {
+            //scoreText is set to score variale to string
             scoreText.text = score.ToString();
         }
 
+        //switch for the movement speed
         switch (score)
         {
             case 100:
--- FoulFiasco/Assets/Scripts/RefManager.cs
+++ FoulFiasco/Assets/Scripts/RefManager.cs
@@ -11,30 +11,42 @@ public class RefManager : MonoBehaviour
 
     private void Start()
     {
+        //playerinfo gets assigned to the playerinfo object
         playerInfo = FindAnyObjectByType<PlayerInfo>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //if the collission tag is wall or ball
         if (collision.tag == "Wall" || collision.tag == "Ball")
         {
+            
             transform.position = new Vector3(-8, transform.position.y, transform.position.z);
+            //an explosion effect gets intantiated
             explosionGameObject = Instantiate(explosion);
+            //explosion position gets set to the collission position
             explosionGameObject.transform.position = collision.transform.position;
+            //collission object gets destroyed
             Destroy(collision.gameObject);
+            //particles gets emitted from the explosion
             StartCoroutine(DestoryParticle());
         }
 
+        //if the collision tag is player
         if (collision.tag == "Player")
         {
+            //score gets set to 0
             playerInfo.score = 0;
+            //main menu gets loaded
             SceneManager.LoadScene("MainMenu");
         }
     }
 
     IEnumerator DestoryParticle()
     {
+        //sets a delay before removing the particles
         yield return new WaitForSeconds(0.6f);
+        //the gameobject with the explosion gets destroyed
         Destroy(explosionGameObject);
     }
 }
