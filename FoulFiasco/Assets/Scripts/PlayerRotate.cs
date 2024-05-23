c28f652bb10e678e50683796f2a20d3bf9c38b5f
Fleur Spannenberg <fleur.spannenberg@gmail.com>
2024-05-23 11:57:38 +0200

    Nieuwe spritesheet

--- FoulFiasco/Assets/Scripts/PlayerRotate.cs
+++ FoulFiasco/Assets/Scripts/PlayerRotate.cs
@@ -6,13 +6,20 @@ public class PlayerRotate : MonoBehaviour
 {
     // Variable for the PlayerInfo script
     PlayerInfo playerInfo;
-  
+    // Variable for the PlayerMovement script
+    PlayerMovement playerMovement;
+    // Variable for the original rotation of the object
+    public Quaternion originalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         // Fetches the PlayerInfo script from the PlayerInfo object
         playerInfo = GameObject.Find("PlayerInfo").GetComponent<PlayerInfo>();
+        // Fetches the PlayerMovement script
+        playerMovement = GetComponent<PlayerMovement>();    
+        // Sets the value of the original rotation
+        originalRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -23,13 +30,22 @@ public class PlayerRotate : MonoBehaviour
 
     void PlayerSpriteRotate()
     {
-        // Fetch the speed from the PlayerInfo script
-        float speed = playerInfo.speed;
+        // If statement that gets excecuted when the player is sliding
+        if (playerMovement.isSliding)
+        {
+            // Resets rotation to the original rotation
+            transform.rotation = originalRotation;
+        }
+        else
+        {
+            // Fetch the speed from the PlayerInfo script
+            float speed = playerInfo.speed;
 
-        // Calculate the rotation angle based on the speed
-        float rotationAngle = speed * Time.deltaTime * -360f;
+            // Calculate the rotation angle based on the speed
+            float rotationAngle = speed * Time.deltaTime * -360f;
 
-        // Apply the rotation to the sprite
-        transform.Rotate(Vector3.forward, rotationAngle);
+            // Apply the rotation to the sprite
+            transform.Rotate(Vector3.forward, rotationAngle);
+        }
     }
 }
