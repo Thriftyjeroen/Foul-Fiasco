1edb2c74a4c7787ca6709221f2d83983ab3eb50e
THERMELINK <thermelink01@student.rocvantwente.nl>
2024-05-23 10:10:04 +0200

    CHORE: comment added to script
    script: buttonmanager added comments in EN

--- FoulFiasco/Assets/Scripts/ButtonManager.cs
+++ FoulFiasco/Assets/Scripts/ButtonManager.cs
@@ -12,13 +12,16 @@ public class ButtonManager : MonoBehaviour
         playerInfo = FindAnyObjectByType<PlayerInfo>();
         if (!(PlayerPrefs.HasKey("Coins"))) PlayerPrefs.SetInt("Coins", 3);
     }
-    //public Scene enumType;//hier word de scene bepaald op basis van de scene die geselecteerd word in de inspector van de gameObject waar dit script aan verbonden is
+    //public scene enumtype, a scene gets selected here at the inspector of the gameObject where this script is assigned to
     public void ChangeScene(string scene)
     {
-        if (!(scene == "Game" && !(PlayerPrefs.GetInt("Coins") > 0)))//zolang de PlayerPrefs (wat
+        if (!(scene == "Game" && !(PlayerPrefs.GetInt("Coins") > 0)))//if the scene is not "game" and the playercoins is not above 0
         {
             gO.SetActive(true);
+            //playerscore gets set to 0
             playerInfo.score = 0;
+
+            //if the scene is "game", a coroutine will start tha loads the scene
             if (scene == "Game")
             {
                 StartCoroutine(LoadScene(scene));
@@ -27,12 +30,18 @@ public class ButtonManager : MonoBehaviour
     }
     IEnumerator LoadScene(string loadScene,int time = 4) 
     {
+        //sets a delay for 4 seconds
         yield return new WaitForSeconds(time);
-        SceneManager.LoadScene(loadScene);//de loadscene van scenemanager laat de aangegeven scene met als input een string of een interger, en sinds er een enum gebruiken gebruiken we de enum die verbonden is met de variabele enumType en daar de waarde van.
-    }
-    public void LeaveGame()//deze method zorgt dat de aplicatie af gesloten kan worden op basis van een knop (wat deze method laat runnen)
+        //loading scene loads the selected scene, with as input a string or interger, but since we use enum, we use the enum that is assigned to the variable enumType and the value of that
+        SceneManager.LoadScene(loadScene);
+    } 
+
+    //this method will close the game, this method gets assigned to a button
+    public void LeaveGame()
     {
+        //sets strings for playerprefs 
         PlayerPrefs.SetString("Running", "false");
+        //will stop the application
         Application.Quit();
     }
     /*public enum Scene//dit is een enumerator, hier kan je een aantal enums(soort variabele) maken met een waarde als integer. die werkt als een array of list, maar het verschil is dat je die ook kan veranderen op de manier die is uitgequote. ook is het zo dat als je een variabele maakt zoals op regel 7 met de naam van de enum en vervolgens het script verbind met een gameObject dat je dan tussen de enums kan kiezen via de inspector
