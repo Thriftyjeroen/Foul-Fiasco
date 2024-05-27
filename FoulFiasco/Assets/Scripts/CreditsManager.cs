8d1dc798a77b44a9fbe59a5d0a5df06b6440b551
Jeroen Verboom <136182490+Thriftyjeroen@users.noreply.github.com>
2024-05-27 14:25:47 +0200

    Fix: Fixed little issues like curtains and missing buttons in main menu

--- FoulFiasco/Assets/Scripts/CreditsManager.cs
+++ FoulFiasco/Assets/Scripts/CreditsManager.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,7 @@ public class CreditsManager : MonoBehaviour
 {
     [SerializeField] TMP_Text credits; // Inits the credits text
     [SerializeField] Camera cam; // Inits the cam
+    float speed = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,15 @@ public class CreditsManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cam.transform.position -= new Vector3(0, 0.005f, 0); // Moves the camera down with 0.01 units
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed = 0.10f;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            speed = 0.01f;
+        }
+        cam.transform.position -= new Vector3(0, speed, 0); // Moves the camera down with 0.01 units
         if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene("MainMenu");
