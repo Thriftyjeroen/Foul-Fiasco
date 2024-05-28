ae05814b3a5ca24b108c23b82ff395f63fc0ad19
Jeroen Verboom <136182490+Thriftyjeroen@users.noreply.github.com>
2024-05-28 12:17:18 +0200

    FEAT: Added sound effects && Fix: Minor bugs

--- FoulFiasco/Assets/Scripts/ScoreInputManager.cs
+++ FoulFiasco/Assets/Scripts/ScoreInputManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.IO;
+using UnityEngine.UI;
 
 public class ScoreInputManager : MonoBehaviour
 {
     [SerializeField] TMP_Text score;
     [SerializeField] TMP_InputField name;
+    [SerializeField] Button button;
     PlayerInfo playerInfo;
     public ScoreUploader scoreUploader;
 
@@ -21,8 +23,18 @@ public class ScoreInputManager : MonoBehaviour
         score.text = $"Score: {playerInfo.score}";
     }
 
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Return) && button.enabled == true)
+        {
+            button.enabled = false;
+            Upload();
+        }
+    }
+
     public void Upload()
     {
+        button.enabled = false;
         if (name.text.Length > 0)
         {
             scoreUploader.Upload(name.text, playerInfo.score);
